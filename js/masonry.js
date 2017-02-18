0861997baacbbf089b38a6bfbbabb57882af26f8
Pranay Kumar Sanam <pranay.sanam@above-inc.com>
2017-02-18 17:02:07 +0530

    masonry ui

---
+++ js/masonry.js
@@ -0,0 +1,56 @@
+class Masonry {
+  constructor(container, items){
+    this.container = container;
+    this.items = [];
+    this.columns = 0;
+    this.colHeights = [];
+    this.itemHeights = {};
+    this.colWidth = 0;
+    this.masonryWidth = 0;
+    this.layoutPositions = {};
+    $(window).resize(this.redoMasonry.bind(this));
+    this.appendItems(items);
+  }
+  appendItems(items) {
+    //$(items).addClass('invisible');
+    items.forEach( item => {
+      const masonryItem = $('<div id="masonry-item-'+items.indexOf(item)+'" class="masonry-item invisible"></div>');
+      $(item).appendTo(masonryItem);
+      this.items.push(masonryItem);
+      this.itemHeights[$(masonryItem).attr('id')]=$(masonryItem).appendTo(this.container).outerHeight();
+    });
+    this.colWidth = $(this.items[0]).outerWidth(true);
+    this.masonry()
+  }
+  masonry() {
+    //$(this.items).addClass('invisible');
+    $(this.container).innerWidth('100%');
+    this.columns = Math.floor($(this.container).innerWidth()/this.colWidth) || 1;
+
+    $(this.container).innerWidth(this.columns*this.colWidth);
+    this.colHeights = [];
+    for(let i=0;i<this.columns;i++){
+      this.colHeights[i] = 0;
+    }
+    this.items.forEach( item => {
+      const colIndex = this.colHeights.indexOf(Math.min.apply(this, this.colHeights));
+      const absLeft = colIndex * this.colWidth;
+      this.layoutPositions[$(item).attr('id')] = {
+        left: absLeft,
+        top: this.colHeights[colIndex]
+      }
+      this.colHeights[colIndex] += this.itemHeights[$(item).attr('id')];
+    });
+    this.redoLayout();
+  }
+  redoLayout() {
+    for(const id in this.layoutPositions) {
+      $('#'+id).css('position','absolute').css('left',this.layoutPositions[id].left).css('top', this.layoutPositions[id].top).removeClass('invisible');
+    }
+  }
+  redoMasonry() {
+    const masonryContainerOW = $(this.container).outerWidth(true);
+    const columns = Math.floor(masonryContainerOW/this.colWidth);
+    if(this.columns !== columns) this.masonry();
+  }
+}
